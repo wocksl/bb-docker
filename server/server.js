af77a90a8b35a9588d8941ea3dbde344f51c7b69
wocksl <wocksl1998@gmail.com>
2024-09-12 15:19:50 +0900

    Proxy setting

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import mysql from 'mysql'
 import express from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors'
-
+import AWS from 'aws-sdk'
 
 //controllers
 //user function handlers
@@ -33,37 +33,71 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-var db = mysql.createConnection({
-  host: "database-1.cluster-cnq6cigc6ady.ap-northeast-2.rds.amazonaws.com",
-  user: "root",
-  password: "j>0G6a:9C*uCHvFCBaEV5}E!E_{.",
-  database: "bbms",
-
+// connect to RDS with Secret Manager
+const secretsManager = new AWS.SecretsManager({
+  region: 'ap-northeast-2'  // AWS 리전 설정
 });
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const getSecret = async (secretName) => {
+  return new Promise((resolve, reject) => {
+    secretsManager.getSecretValue({ SecretId: secretName }, (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        if ('SecretString' in data) {
+          resolve(JSON.parse(data.SecretString));
+        } else {
+          reject('No secret string found');
+        }
+      }
+    });
+  });
+};
 
-//user functionalities
-UserRegisterHandler(app, db);
-UserLoginHandler(app, db);
-RequestClassHandler(app, db);
+// 서버 시작 시 Secrets Manager에서 DB 연결 정보 가져오기
+(async () => {
+  try {
+    // AWS Secrets Manager에서 설정한 비밀 이름
+    const secretName = "bbms/mysql";  // AWS Secrets Manager에 저장한 비밀 이름
+    const secret = await getSecret(secretName);
 
-//employee functionalities
-EmployeeRegisterHandler(app, db);
-EmployeeLoginHandler(app, db);
-UpdateHealthHandler(app, db);
-HandleRequestHandler(app, db);
+    // MySQL 연결 설정
+    var db = mysql.createConnection({
+      host: secret.host,
+      user: secret.username,
+      password: secret.password,
+      database: secret.dbname,
+    });
 
-//bloodbank functionalities
-DashboardHandler(app, db);
-UpdateBlood(app, db);
-SearchHandler(app, db);
+    db.connect((err) => {
+      if (err) throw err;
+      console.log("Connected to the database!");
 
-//listening the port
-app.listen(3001, (err) => {
-  if (err) throw err;
-  else console.log("listening to port : 3001");
-});
+      // 이후 라우팅 및 핸들러 등록
+      // user functionalities
+      UserRegisterHandler(app, db);
+      UserLoginHandler(app, db);
+      RequestClassHandler(app, db);
+
+      // employee functionalities
+      EmployeeRegisterHandler(app, db);
+      EmployeeLoginHandler(app, db);
+      UpdateHealthHandler(app, db);
+      HandleRequestHandler(app, db);
+
+      // bloodbank functionalities
+      DashboardHandler(app, db);
+      UpdateBlood(app, db);
+      SearchHandler(app, db);
+
+      // 서버 리스닝
+      app.listen(3001, (err) => {
+        if (err) throw err;
+        else console.log("Listening to port 3001");
+      });
+    });
+  } catch (error) {
+    console.error('Error fetching secrets from AWS Secrets Manager:', error);
+  }
+})();
